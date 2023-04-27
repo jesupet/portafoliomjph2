299c52ee60dde04277bfb418149c5b81dfa96ac7
MariaJesús <jesup93@gmail.com>
2023-04-27 17:12:15 -0400

    NAVBAR

---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="navbar navbar-dark navbar-expand-lg">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto align-items-center">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" :to="{name: 'home', hash: '#'+'aboutme'}">About Me</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" :to="{name: 'home', hash: '#'+'proyectos'}">My Projects</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" :to="{name: 'home', hash: '#'+'toolkit'}">Tool Kit</router-link>
+          </li>
+          <li>
+            <PurpleButton :buttonText="'Write Me!'"/>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import PurpleButton from './PurpleButton.vue'
+
+export default {
+  name: 'NavBar',
+  components: {
+    PurpleButton,
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/PurpleButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <button type="button" class="btn btn-purple rounded-pill">
+    <router-link :to="to">{{buttonText}}</router-link>
+  </button>
+</template>
+
+<script>
+
+export default {
+  name: 'PurpleButton',
+  props: {
+    to: {
+      type: String,
+      default: '/'
+    },
+    buttonText: {
+      type: String,
+      default: 'Button'
+    },
+    href: {
+      type: String,
+      default:'/'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.btn-purple {
+  background-color: #5241B8;
+  color: #fff;
+  font-size: 14px;
+  padding: 8px 20px 8px 20px;
+}
+button:hover {
+  text-decoration: none;
+  color: #5241B8;
+  background-color: #fff;
+  border-color:#5241B8;
+}
+
+
+</style>
