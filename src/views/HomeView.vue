60f79dc5e4144df9c84565a7148ef1e5451b8e26
MariaJesús <jesup93@gmail.com>
2023-04-27 17:41:14 -0400

    HEADER

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template>
+<div class="background-image">
   <NavBar/>
+  <header class="container align-items-center">
+    <div>
+      <h1>Hello my name is Maria. <span>Welcome to my portfolio!</span></h1>
+    </div>
+  </header>
+</div>
+  
 </template>
 
 <script>
