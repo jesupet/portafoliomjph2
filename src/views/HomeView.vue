fdcd89ba24a647a83174a371a5be61101787d4ab
MariaJesús <jesup93@gmail.com>
2023-05-02 19:15:13 -0400

    PROYECTOS Y TOOLKIT

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,6 +23,77 @@
       </div>
     </div>
   </section>
+  <section id="myprojects" class="container-fluid">
+    <h2>My Projects</h2>
+    <div class="row">
+      <div class="col-12 col-sm-6 proy-cont">
+        <div class="box">
+          <img src="@/assets/img/eleamchile.png" alt="" class="img-fluid">
+          <div class="overlay text-center d-flex justify-content-center flex-column align-items-center">
+            <PurpleButton :buttonText="'Eleam Chile'" :href="'https://eleamchile.cl'"/>
+            <p>Project Manager / Diseño UX UI</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-6 proy-cont">
+        <div class="box">
+          <img src="@/assets/img/satournow.png" alt="" class="img-fluid">
+          <div class="overlay text-center d-flex justify-content-center flex-column align-items-center">
+            <PurpleButton :buttonText="'Sa-tour-now!'" :href="'https://jesupet.github.io/SaturnoGit/'"/>
+            <p>Final Project Front End Career</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-6 proy-cont">
+        <div class="box">
+          <img src="@/assets/img/futuropatagonia.png" alt="" class="img-fluid">
+          <div class="overlay text-center d-flex justify-content-center flex-column align-items-center">
+            <PurpleButton :buttonText="'Futuro Patagonia'" :href="'https://futuropatagonia.cl/'"/>
+            <p>Front End Developer with Vanilla</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-6 proy-cont">
+        <div class="box">
+          <img src="@/assets/img/vanilla.png" alt="" class="img-fluid">
+          <div class="overlay text-center d-flex justify-content-center flex-column align-items-center">
+            <PurpleButton :buttonText="'Vanilla (Soon)'"/>
+            <p>Front End Developer with Vanilla</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="toolkit" class="container text-center">
+    <h2>Tool Kit</h2>
+    <div class="tool-box">
+      <div class="text-center mx-2">
+        <img src="@/assets/img/tool_kit_logos/js.png" alt="javascript logo" class="img-fluid tool-logo">
+      </div>
+      <div class="text-center mx-2">
+        <img src="@/assets/img/tool_kit_logos/css.png" alt="css logo" class="img-fluid tool-logo">
+      </div>
+      <div class="text-center mx-2">
+        <img src="@/assets/img/tool_kit_logos/vue.png" alt="vue logo" class="img-fluid tool-logo">
+      </div>
+      <div class="text-center mx-2">
+        <img src="@/assets/img/tool_kit_logos/sass.png" alt="sass logo" class="img-fluid tool-logo">
+      </div>
+      <div class="text-center mx-2">
+        <img src="@/assets/img/tool_kit_logos/bootstrap.png" alt="bootstrap logo" class="img-fluid tool-logo">
+      </div>
+      <div class="text-center mx-2">
+        <img src="@/assets/img/tool_kit_logos/figma.png" alt="bootstrap logo" class="img-fluid tool-logo">
+      </div>
+      <div class="text-center mx-2">
+        <img src="@/assets/img/tool_kit_logos/trello.png" alt="bootstrap logo" class="img-fluid tool-logo">
+      </div>
+      <div class="text-center mx-2">
+        <img src="@/assets/img/tool_kit_logos/notion.svg.png" alt="bootstrap logo" class="img-fluid tool-logo">
+      </div>
+    </div>
+    
+  </section>
 </div>
   
 </template>
