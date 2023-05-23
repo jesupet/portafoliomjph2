45c969c7d8f51a88dc5a6e4e2552234771e6bcc1
MariaJesús <jesup93@gmail.com>
2023-05-23 14:52:27 -0400

    ARREGLADO NAVBAR Y AGREGADO GOOGLE TAG

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,14 @@
 <template>
 <nav class="navbar navbar-dark navbar-expand-lg">
     <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="navbarNav">
+        <div class="text-end p-4">
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <ul class="navbar-nav ms-lg-auto align-items-center">
           <li class="nav-item">
             <a class="nav-link" aria-current="page" href="#">Home</a>
           </li>
@@ -19,38 +22,12 @@
             <router-link class="nav-link" aria-current="page" :to="{name: 'home', hash: '#'+'toolkit'}">Tool Kit</router-link>
           </li>
           <li>
-            <router-link class="btn btn-purple rounded-pill" aria-current="page" :to="{name: 'home', hash: '#'+'contact'}">Write Me!</router-link>
+            <router-link class="btn btn-purple rounded-pill btn-nav" aria-current="page" :to="{name: 'home', hash: '#'+'contact'}">Write Me!</router-link>
           </li>
         </ul>
       </div>
     </div>
   </nav>
-  <!--<nav class="navbar navbar-dark navbar-expand-lg">
-    <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto align-items-center">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">Inicio</a>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" aria-current="page" :to="{name: 'home', hash: '#'+'aboutme'}">About Me</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" aria-current="page" :to="{name: 'home', hash: '#'+'myprojects'}">My Projects</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" aria-current="page" :to="{name: 'home', hash: '#'+'toolkit'}">Tool Kit</router-link>
-          </li>
-          <li>
-            <router-link class="btn btn-purple rounded-pill" aria-current="page" :to="{name: 'home', hash: '#'+'contact'}">Write Me!</router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>-->
 </template>
 
 <script>
